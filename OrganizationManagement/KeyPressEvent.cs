692c7c2ee35527956e43c552414f4054f7db74e8
Эльза Устименко <elza-usti2012@yandex.ru>
2023-11-11 15:10:12 +0300

    события для обработки ввода в textbox

---
+++ OrganizationManagement/KeyPressEvent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationManagement
+{
+    public class KeyPressEvent
+    {
+        public static void textBox_KeyPressMoney(object sender, KeyPressEventArgs e)
+        {
+            // Проверяем, является ли введенный символ цифрой или запятой
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true; // Отменяем ввод, если символ не является цифрой или запятой
+            }
+
+            // Проверяем, что введена только одна запятая
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+
+            // Проверяем, что после запятой не более двух цифр
+            if ((sender as TextBox).Text.Contains(','))
+            {
+                string[] parts = (sender as TextBox).Text.Split(',');
+                if (parts.Length > 1 && parts[1].Length >= 2 && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        public static void textBox_KeyPressPercent(object sender, KeyPressEventArgs e)
+        {
+            // Проверка, является ли введенный символ цифрой, Backspace или Delete
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Отклонить ввод, если это не цифра, Backspace или Delete
+            }
+
+            // Если введенный символ - цифра
+            if (char.IsDigit(e.KeyChar))
+            {
+                string newText = (sender as TextBox).Text + e.KeyChar;
+                int value;
+                if (!int.TryParse(newText, out value) || value < 0 || value > 100)
+                {
+                    e.Handled = true; // Отклонить ввод, если число не в пределах от 0 до 100
+                }
+            }
+
+        }
+    }
+}

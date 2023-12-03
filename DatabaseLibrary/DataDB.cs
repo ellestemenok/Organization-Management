a0d366cb922ce9cb510dbb382f73e46729248335
Эльза Устименко <elza-usti2012@yandex.ru>
2023-12-03 13:13:21 +0300

    s

--- DatabaseLibrary/DataDB.cs
+++ DatabaseLibrary/DataDB.cs
@@ -5,9 +5,6 @@ using Npgsql.Internal;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DatabaseLibrary
